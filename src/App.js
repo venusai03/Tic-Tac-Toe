11631a535aa236c0033c7ddfbf3d7a81146a56fa
Venu Sai Madisetti <venu@betacrew.io>
2023-10-02 13:10:45 +0530

    add tic-tac-toe game

--- src/App.js
+++ src/App.js
@@ -1,21 +1,130 @@
-function App() {
+import "./App.css"
+import {useState} from "react";
+
+function Square({value, onSquareClick}) {
     return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <button className="square" onClick={onSquareClick}>{value}</button>
+    );
+}
+
+function Board({xTurn, squares, onPlay}) {
+    const winner = calculateWin(squares);
+    let status;
+
+    if (winner) {
+        status = winner + " is the winner!";
+    } else {
+        if (!squares.includes(null)) {
+            status = "It's a Draw!";
+        } else {
+            status = (xTurn ? "X" : "O") + "'s turn";
+        }
+    }
+
+    function handleClick(i) {
+        if (squares[i] || calculateWin(squares)) {
+            return;
+        }
+
+        const nextSquares = squares.slice();
+
+        if (xTurn) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+
+        onPlay(nextSquares);
+    }
+
+    return (
+        <>
+            <p>{status}</p>
+            <div className="board-row">
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+            </div>
+            <div className="board-row">
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+            </div>
+        </>
+    );
+}
+
+function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+
+    const currentSquares = history[currentMove];
+    const xTurn = currentMove % 2 === 0;
+
+    function handlePlay(squares) {
+        const nextHistory = [...history.slice(0, currentMove + 1), squares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(move) {
+        setCurrentMove(move);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+
+        if (move === 0) {
+            description = "Go to start";
+        } else {
+            description = "Go to move #" + move;
+        }
+
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        )
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xTurn={xTurn} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
         </div>
     );
 }
 
-export default App;
+function calculateWin(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+
+    return null;
+}
+
+export default Game;
